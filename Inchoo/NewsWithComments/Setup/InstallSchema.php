5135739c4fce7361d62793d39776ca2e399c8e07
Matej Maricic <matej.maricic@gmail.com>
2019-05-13 09:49:52 +0200

    Adding InstallSchema

---
+++ Inchoo/NewsWithComments/Setup/InstallSchema.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Inchoo\NewsWithCommments\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $news = $setup->getConnection()->newTable(
+            $setup->getTable('inchoo_news')
+        )->addColumn(
+            'news_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+            'News Id'
+        )->addColumn(
+            'title',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            [],
+            'News Title'
+        )->addColumn(
+            'content',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            [],
+            'News Content'
+        )->addColumn(
+            'added_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT,
+            ],
+            'Added at'
+        )->addColumn(
+            'added_by',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'unsigned' => true,
+                'nullable' => false
+            ],
+            'Added By'
+        )->addForeignKey(
+            $setup->getFkName('inchoo_news', 'added_by', 'admin_user', 'user_id'),
+            'added_by',
+            $setup->getTable('admin_user'),
+            'user_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        );
+
+        $comments = $setup->getConnection()->newTable(
+            $setup->getTable('inchoo_comments')
+        )->addColumn(
+            'comment_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+            'Comment Id'
+        )->addColumn(
+            'comment_content',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            [],
+            'Comment Content'
+        )->addColumn(
+            'comment_added_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT,
+            ],
+            'Comment Added at'
+        )->addColumn(
+            'comment_added_by',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [
+                'unsigned' => true,
+                'nullable' => false
+            ],
+            'Comment Added By'
+        )->addForeignKey(
+            $setup->getFkName('inchoo_comments', 'comment_added_by', 'customer_entity', 'entity_id'),
+            'comment_added_by',
+            $setup->getTable('customer_entity'),
+            'entity_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        );
+
+        $setup->getConnection()->createTable($news);
+        $setup->getConnection()->createTable($comments);
+
+        $setup->endSetup();
+    }
+}
