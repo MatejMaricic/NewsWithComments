4937489848f8d85a66565a07e1d0811c6bbf2492
Matej Maricic <matej.maricic@gmail.com>
2019-05-16 15:51:13 +0200

    created all news page and added pagination, still need filters

---
+++ Inchoo/NewsWithComments/Controller/Index/View.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Inchoo\NewsWithComments\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+
+class View extends Action
+{
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    private $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
