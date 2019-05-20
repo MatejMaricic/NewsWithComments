a887a5c3331c1ce72be3b5d09bea1aa6856cf912
Matej Maricic <matej.maricic@gmail.com>
2019-05-20 09:31:01 +0200

    check store view on single news page

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -12,19 +12,35 @@ class Index extends Action
      * @var \Magento\Framework\View\Result\PageFactory
      */
     private $resultPageFactory;
+    /**
+     * @var \Inchoo\NewsWithComments\Api\NewsRepositoryInterface
+     */
+    private $newsRepository;
+    /**
+     * @var Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManagerInterface;
 
     public function __construct(
         Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Inchoo\NewsWithComments\Api\NewsRepositoryInterface $newsRepository,
+        \Magento\Store\Model\StoreManagerInterface $storeManagerInterface
     ) {
         parent::__construct($context);
 
         $this->resultPageFactory = $resultPageFactory;
+        $this->newsRepository = $newsRepository;
+        $this->storeManagerInterface = $storeManagerInterface;
     }
 
     public function execute()
     {
-        if ($this->_request->getParam('id')) {
+        $news = $this->newsRepository->getById($this->_request->getParam('id'));
+        $view = $news->getStoreView();
+
+        if ($this->storeManagerInterface->getStore()->getId() == $view) {
+
             $resultPage = $this->resultPageFactory->create();
             return $resultPage;
         }
